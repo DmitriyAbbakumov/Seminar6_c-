c497c2369510e37f426607d15296a9aa4f35cf0c
Dmitriy <simpa-baki@mail.ru>
2022-08-24 20:20:00 +0300

    first commit

---
+++ Ex41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, 
+// сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+int[] FillArray (int m)         //Ввод чисел и заполнение массива
+{
+    int[] array = new int[m];
+    for (int i = 0; i < m; i++)
+    {
+        Console.WriteLine($"Введите {i + 1} число: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+int PrintArrayCounter(int[] array)    //Печать массива и счетчик нужных чисел
+{
+    int counter = 0;
+    Console.Write("Вы ввели: [ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0) counter++;
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine("] ");
+    return counter;
+}
+Console.Clear();
+Console.WriteLine("Введите количество цифр, которые будете вводить:");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] arr = FillArray(M);
+int count = PrintArrayCounter(arr);
+Console.WriteLine($"Чисел больше нуля: {count}");
---
+++ Ex43/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("введите k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+double x = 0;
+double y = 0;
+
+void result(double ex, double ey, double ek1, double ek2, double eb1, double eb2)
+{
+    if (ek1 == ek2 && eb1 != eb2)
+    {
+        Console.WriteLine("прямые параллельны, нет точек пересечения");
+    }
+    else if (ek1 == ek2 && eb1 == eb2) Console.WriteLine("прямые совпадают");
+    else
+    {
+        ex = (eb2 - eb1) / (ek1 - ek2);
+        ey = (ek1 * eb2 - ek2 * eb1) / (ek1 - ek2);
+        Console.WriteLine($"({ex}; {ey})");
+    }
+}
+result(x, y, k1, k2, b1, b2);
